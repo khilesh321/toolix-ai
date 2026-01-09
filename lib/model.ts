25cbf1efd54c1d2c603c0b44e7c16b2ea19fca42
Khilesh Jawale <khilujawale321@gmail.com>
2026-01-09 14:48:46 +0530

    add web search tool functionality

--- lib/model.ts
+++ lib/model.ts
@@ -1,7 +1,8 @@
 import { ChatGroq } from "@langchain/groq";
 import { weatherTool } from "@/lib/tools/weather";
+import { webSearchTool } from "./tools/webSearch";
 
-export const tools = [weatherTool];
+export const tools = [weatherTool, webSearchTool];
 
 export const model = new ChatGroq({
   model: "openai/gpt-oss-120b",
---
+++ lib/tools/webSearch.ts
@@ -0,0 +1,36 @@
+import { tool, ToolRuntime } from "@langchain/core/tools";
+import { TavilySearch } from "@langchain/tavily";
+import z from "zod";
+
+const webSearch = new TavilySearch({
+  maxResults: 3,
+  topic: "general",
+  includeImages: false,
+});
+
+export const webSearchTool = tool(
+  async ({ query }: { query: string }, config:ToolRuntime) => {
+
+    config.writer?.({
+      type: "progress",
+      id: "web_search",
+      message: `Searching the web for "${query}"...`,
+    })
+    const res = await webSearch.invoke({ query });
+
+    config.writer?.({
+      type: "progress",
+      id: "web_search",
+      message: "Web search completed successfully",
+    });
+    return res;
+  },
+  {
+    name: "web_search",
+    description:
+      "Useful for when you need to answer questions about current events or find specific information from the web.",
+    schema: z.object({
+      query: z.string().describe("The search query to look up on the web"),
+    }),
+  }
+);
